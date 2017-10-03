f5ce8f85b34bce21d2db5ac5c56d18ed2fe266c4
mattgdev69 <32312016+mattgdev69@users.noreply.github.com>
2017-10-03 15:58:22 -0500

    Add files via upload

--- adventure.py
+++ adventure.py
@@ -9,7 +9,7 @@ def main():
     # Open database and initialize location
     conn = sqlite3.connect("adventure.sqlite")
     c = conn.cursor()
-    locid = 1
+    # locid = 1
 
     # Create a list of weapon objects from weapon table
     c.execute('SELECT * FROM {tn}'.
@@ -21,38 +21,45 @@ def main():
         weapon.store(w)
         weapons.append(weapon)
 
+    # Create a list of location objects from location table
+    # Set current_loc to first location
+    c.execute('SELECT * FROM {tn}'.
+              format(tn="World"))
+    rows = c.fetchall()
+    locations = []
+    for l in rows:
+        loc = Location()
+        loc.store(l, weapons)
+        locations.append(loc)
+    current_loc = locations[0]
+
     # initialize player character
-    player = Player()
-    player.weapon_equipped = weapons[0]
+    player = Player(weapons)
     print("Hello", player.name)
 
+    # Main loop
     while True:
 
-        # Fetch location row from database and create location object
-        c.execute('SELECT * FROM {tn} WHERE {cn}=locid'.
-                  format(tn="World", cn=locid))
-        loc = Location()
-        row = c.fetchone()
-        loc.store(row)
-
-        # Check for Encounter and create enemy (20% chance)
-        if (random.randint(1, 100)) < 20:
+        # There is a 20% chance an enemy will appear in the current area if
+        # not already present
+        if (random.randint(1, 100)) < 20 and len(current_loc.enemies) == 0:
             c.execute('SELECT * FROM {tn} ORDER BY RANDOM() LIMIT 1'.
                       format(tn="Enemy"))
             row = c.fetchone()
             enemy = Enemy()
             enemy.store(row)
-            loc.enemies.append(enemy)
-            print("Enemy :", loc.enemies[0].name)
+            current_loc.enemies.append(enemy)
 
         # Show world details and prompt for action
         print()
-        print('Location:  You are in the', loc.name)
+        print('Location:  You are in the', current_loc.name)
         print()
-        print(loc.desc)
-        if len(loc.enemies) > 0:
-            print("Enemy encountered in this location!  You see a", loc.enemies[0].name)
-
+        print(current_loc.desc)
+        print()
+        if len(current_loc.weapons) > 0:
+            print("You see a", current_loc.weapons[0].name, "in this area.")
+        if len(current_loc.enemies) > 0:
+            print("Enemy encountered in this location!  You see a", current_loc.enemies[0].name)
         print()
         print("You have", player.hp, "health.")
         action = input("What next? ")
@@ -60,19 +67,42 @@ def main():
 
         # Travel
         if action in ["north", "south", "east", "west"]:
-            nextloc = getattr(loc, action)
+            nextloc = getattr(current_loc, action)
 
+            # Set current location to new location traveled
             if nextloc > 0:
-                locid = nextloc
+                for l in locations:
+                    if l.locid == nextloc:
+                        current_loc = l
             else:
                 print()
                 print("You can't go that way!")
-
             continue
+
+        # Get weapon.  Add it to player inventory and remove from location.
+        if action[:3] == "get":
+            weapon = action[4:]
+            if current_loc.weapons[0].name.lower() == weapon:
+                player.weapons.append(current_loc.weapons[0])
+                del current_loc.weapons[0]
+                print("You picked up the", weapon)
+                print()
+            else:
+                print("You can't do that!")
+                print()
+
+        # Equip weapon in inventory
+        if action[:5] == "equip":
+            weapon = action[6:]
+            player.equip(weapon)
+
+        # List player inventory
+        if action[:3] == "inv":
+            player.inventory
 
         # Attack
         if action == "attack":
-            Attack(player, loc.enemies[0])
+            Attack(player, current_loc)
             if player.hp <= 0:
                 action = "q"
                 print(player.name, "has died!")
@@ -84,8 +114,9 @@ def main():
 
 
 # Combat routine
-def Attack(player, enemy):
+def Attack(player, loc):
 
+    enemy = loc.enemies[0]
     while enemy.hp > 0 and player.hp > 0:
 
         # Player attacks
@@ -101,6 +132,7 @@ def Attack(player, enemy):
                 print(enemy.name, "has", enemy.hp, "HP remaining.")
             else:
                 print("The", enemy.name, "is dead!")
+                del loc.enemies[0]
                 break
             print()
         else:
@@ -142,11 +174,11 @@ class Location:
         self.CONST_SOUTH = 4
         self.CONST_EAST = 5
         self.CONST_WEST = 6
+        self.CONST_WEAPONS = 7
         self.weapons = []
-        self.items = []
         self.enemies = []
 
-    def store(self, row):
+    def store(self, row, weapons):
         self.locid = row[self.CONST_LOCID]
         self.name = row[self.CONST_LOCATION]
         self.desc = row[self.CONST_DESC]
@@ -154,16 +186,29 @@ class Location:
         self.south = row[self.CONST_SOUTH]
         self.east = row[self.CONST_EAST]
         self.west = row[self.CONST_WEST]
+        for w in weapons:
+            if w.id == row[self.CONST_WEAPONS]:
+                self.weapons.append(w)
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, weapons):
         self.name = input("Enter your player's name: ")
         self.hp = 100
         self.ac = 20
-        self.inv = []
-        self.weapons = [1]
-        self.weapon_equipped = []
+        self.weapons = []
+        for w in weapons:
+            if w.id == 1:
+                self.weapons.append(w)
+        self.weapon_equipped = self.weapons[0]
+
+    def equip(self, weapon):
+        for w in self.weapons:
+            if w.name.lower() == weapon:
+                self.weapon_equipped = w
+            else:
+                print("Weapon not in inventory!")
+                print()
 
 
 class Enemy:
@@ -186,12 +231,6 @@ class Enemy:
         self.hp = row[self.CONST_HP]
         self.ac = row[self.CONST_AC]
         self.damage = row[self.CONST_DAMAGE]
-
-#    def spawn(self, c):
-#        c.execute('SELECT * FROM {tn} ORDER BY RAND() LIMIT 1'.
-#                  format(tn="Enemy"))
-#        row = c.fetchone()
-#        print("Enemy :", row)
 
 
 class Weapon:
